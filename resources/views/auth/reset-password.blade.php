9a185e1a9f18e38d55d8fa40cb37d0f76f01930c
Shakeel Ahmed <shakeel2717@gmail.com>
2022-02-17 17:22:57 +0500

    Forget Rest Password Template Update

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -3,16 +3,58 @@
     Login Page
 @endsection
 @section('form')
-    <form class="js-validate" method="POST" action="{{ route('password.update') }}">
-        @csrf
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
-        <div class="text-center">
-            <h1 class="display-4">Reset Password!</h1>
-            <p>Please Choose a new password for your account.</p>
+    <div class="block-content block-content-full px-lg-5 px-xl-6 py-4 py-md-5 py-lg-6 bg-white">
+        <div class="mb-2 text-center">
+            <a class="link-fx font-w700 font-size-h1" href="index.html">
+                <span class="text-dark">Dash</span><span class="text-primary">mix</span>
+            </a>
+            <div class="d-flex flex-column">
+                <a href="{{ route('login') }}">Remember Password?</a>
+                <hr>
+                <p class="text-uppercase font-w700 font-size-sm text-muted">Reset Password!</p>
+                <p>Please Choose a new password for your account.</p>
+            </div>
         </div>
-        <x-input name="email" type="email" placeholder="Enter Email." value="{{ old('email', $request->email) }}" />
-        <x-input name="password" type="password" placeholder="Choose new Password." value="{{ old('password') }}" />
-        <x-input name="password_confirmation" type="password" placeholder="Confirm Password." value="{{ old('password') }}" />
-        <button type="submit" class="btn btn-lg btn-block btn-primary">Reset Password</button>
-    </form>
+        <form class="js-validation-signin" action="{{ route('password.update') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <div class="input-group">
+                    <input type="text" class="form-control" id="email" name="email"
+                        placeholder="Email" value="{{ old('email', $request->email) }}">
+                    <div class="input-group-append">
+                        <span class="input-group-text">
+                            <i class="fa fa-user-circle"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="input-group">
+                    <input type="password" class="form-control" id="password" name="password"
+                        placeholder="New Password">
+                    <div class="input-group-append">
+                        <span class="input-group-text">
+                            <i class="fa fa-asterisk"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="input-group">
+                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation"
+                        placeholder="Confirm new Password">
+                    <div class="input-group-append">
+                        <span class="input-group-text">
+                            <i class="fa fa-asterisk"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group text-center">
+                <button type="submit" class="btn btn-hero-primary">
+                    <i class="fa fa-fw fa-sign-in-alt mr-1"></i> Reset Password
+                </button>
+            </div>
+        </form>
+    </div>
 @endsection
