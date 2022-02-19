bd7a91f5f14343dd130d65194e625b31d13e5e11
Shakeel Ahmed <shakeel2717@gmail.com>
2022-02-19 19:17:01 +0500

    Dashboard update

--- resources/views/user/dashboard/index.blade.php
+++ resources/views/user/dashboard/index.blade.php
@@ -1,5 +1,147 @@
-<h2 class="title">Welcome User to Dashboard</h2>
-<form action="{{ route('logout') }}" method="POST">
-    @csrf
-    <button type="submit" class="btn btn-primary">Logout</button>
-</form>
+@extends('user.layout.app')
+@section('title')
+    Dashboard
+@endsection
+
+@section('content')
+    <div class="content">
+        <h2 class="content-heading">
+            <i class="fa fa-angle-right text-muted mr-1"></i> Quick Overview
+        </h2>
+        <div class="block block-rounded invisible" data-toggle="appear">
+            <div class="block-content block-content-full">
+                <div class="row text-center">
+                    <div class="col-md-4 py-3">
+                        <div class="font-size-h1 font-w300 text-black mb-1">
+                            $0.00
+                        </div>
+                        <a class="link-fx font-size-sm font-w700 text-uppercase text-muted"
+                            href="javascript:void(0)">Balance</a>
+                    </div>
+                    <div class="col-md-4 py-3">
+                        <div class="font-size-h1 font-w300 text-success mb-1">
+                            +$0.00
+                        </div>
+                        <a class="link-fx font-size-sm font-w700 text-uppercase text-muted" href="javascript:void(0)">Income
+                            Today</a>
+                    </div>
+                    <div class="col-md-4 py-3">
+                        <div class="font-size-h1 font-w300 text-danger mb-1">
+                            -$0.00
+                        </div>
+                        <a class="link-fx font-size-sm font-w700 text-uppercase text-muted"
+                            href="javascript:void(0)">Overall Withdraw</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h2 class="content-heading">
+            <i class="fa fa-angle-right text-muted mr-1"></i> Finance Manage
+        </h2>
+        <div class="row">
+            <div class="col-lg-6 js-appear-enabled animated fadeIn" data-toggle="appear">
+                <a class="block block-rounded block-link-shadow" href="javascript:void(0)">
+                    <div class="block-content block-content-full d-flex align-items-center justify-content-between">
+                        <div>
+                            <p class="font-size-lg font-w600 mb-0">
+                                $ <span class="text-default">0.00</span>
+                            </p>
+                            <p class="text-muted mb-0">
+                                Refer Commission
+                            </p>
+                        </div>
+                        <div class="ml-3">
+                            <i class="fa fa-piggy-bank fa-2x text-gray"></i>
+                        </div>
+                    </div>
+                    <div class="block-content block-content-full block-content-sm text-center bg-body-light">
+                        <span class="font-size-sm text-muted">View All Transactions</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-6 js-appear-enabled animated fadeIn" data-toggle="appear">
+                <a class="block block-rounded block-link-shadow" href="javascript:void(0)">
+                    <div class="block-content block-content-full d-flex align-items-center justify-content-between">
+                        <div>
+                            <p class="font-size-lg font-w600 mb-0">
+                                $ <span class="text-default">0.00</span>
+                            </p>
+                            <p class="text-muted mb-0">
+                                Team Business Reward
+                            </p>
+                        </div>
+                        <div class="ml-3">
+                            <i class="fa fa-piggy-bank fa-2x text-gray"></i>
+                        </div>
+                    </div>
+                    <div class="block-content block-content-full block-content-sm text-center bg-body-light">
+                        <span class="font-size-sm text-muted">View All Transactions</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-6 js-appear-enabled animated fadeIn" data-toggle="appear">
+                <a class="block block-rounded block-link-shadow" href="javascript:void(0)">
+                    <div class="block-content block-content-full d-flex align-items-center justify-content-between">
+                        <div>
+                            <p class="font-size-lg font-w600 mb-0">
+                                $ <span class="text-default">0.00</span>
+                            </p>
+                            <p class="text-muted mb-0">
+                                Direct Sale Reward
+                            </p>
+                        </div>
+                        <div class="ml-3">
+                            <i class="fa fa-piggy-bank fa-2x text-gray"></i>
+                        </div>
+                    </div>
+                    <div class="block-content block-content-full block-content-sm text-center bg-body-light">
+                        <span class="font-size-sm text-muted">View All Transactions</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-6 js-appear-enabled animated fadeIn" data-toggle="appear">
+                <a class="block block-rounded block-link-shadow" href="javascript:void(0)">
+                    <div class="block-content block-content-full d-flex align-items-center justify-content-between">
+                        <div>
+                            <p class="font-size-lg font-w600 mb-0">
+                                $ <span class="text-default">0.00</span>
+                            </p>
+                            <p class="text-muted mb-0">
+                                Direct Sale Reward
+                            </p>
+                        </div>
+                        <div class="ml-3">
+                            <i class="fa fa-piggy-bank fa-2x text-gray"></i>
+                        </div>
+                    </div>
+                    <div class="block-content block-content-full block-content-sm text-center bg-body-light">
+                        <span class="font-size-sm text-muted">View All Transactions</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <h2 class="content-heading">
+            <i class="fa fa-angle-right text-muted mr-1"></i> Latest Transactions
+        </h2>
+        <a class="block block-rounded block-link-shadow border-left border-success border-3x js-appear-enabled animated fadeIn"
+            data-toggle="appear" href="javascript:void(0)">
+            <div class="block-content block-content-full d-flex align-items-center justify-content-between">
+                <div>
+                    <p class="font-size-lg font-w600 mb-0">
+                        +$250,00
+                    </p>
+                    <p class="text-muted mb-0">
+                        xxx-485 Account
+                    </p>
+                </div>
+                <div class="ml-3">
+                    <i class="fa fa-arrow-left text-success"></i>
+                </div>
+            </div>
+            <div class="block-content block-content-full block-content-sm bg-body-light">
+                <span class="font-size-sm text-muted">From <strong>Company Inc</strong> at <strong>June 10, 2018 -
+                        10:06</strong></span>
+            </div>
+        </a>
+    </div>
+@endsection
