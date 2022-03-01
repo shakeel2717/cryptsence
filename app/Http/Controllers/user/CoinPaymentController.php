e9a6e5bfec729cd9bba5f4147ef4648c2d7028d5
Shakeel Ahmed <shakeel2717@gmail.com>
2022-03-01 07:18:27 +0500

    POST convert into request

--- app/Http/Controllers/user/CoinPaymentController.php
+++ app/Http/Controllers/user/CoinPaymentController.php
@@ -17,12 +17,12 @@ class CoinPaymentController extends Controller
         $merchant_id = env('COINPAYMENTSMERCHANT');
         $ipn_secret = env('IPN_SECRET');
         Log::info('CoinPayment webhook  Init');
-        $txn_id = $_POST['txn_id'];
+        $txn_id = $request->txn_id;
         $payment = btcPayments::where("txn_id", $txn_id)->first();
         $order_currency = $payment->to_currency; //BTC
         $order_total = $payment->amount; //BTC
 
-        if (!isset($_POST['ipn_mode']) || $_POST['ipn_mode'] != 'hmac') {
+        if (!isset($request->ipn_mode) || $request->ipn_mode != 'hmac') {
             edie("IPN Mode is not HMAC");
         }
 
@@ -30,25 +30,25 @@ class CoinPaymentController extends Controller
             edie("No HMAC Signature Sent.");
         }
 
-        $request = file_get_contents('php://input');
-        if ($request === false || empty($request)) {
+        $requestFile = file_get_contents('php://input');
+        if ($requestFile === false || empty($requestFile)) {
             edie("Error in reading Post Data");
         }
 
-        if (!isset($_POST['merchant']) || $_POST['merchant'] != trim($merchant_id)) {
+        if (!isset($request->merchant) || $request->merchant != trim($merchant_id)) {
             edie("No or incorrect merchant id.");
         }
 
-        $hmac =  hash_hmac("sha512", $request, trim($ipn_secret));
+        $hmac =  hash_hmac("sha512", $requestFile, trim($ipn_secret));
         if (!hash_equals($hmac, $_SERVER['HTTP_HMAC'])) {
             edie("HMAC signature does not match.");
         }
 
-        $amount1 = floatval($_POST['amount1']); //IN USD
-        $amount2 = floatval($_POST['amount2']); //IN BTC
-        $currency1 = $_POST['currency1']; //USD
-        $currency2 = $_POST['currency2']; //BTC
-        $status = intval($_POST['status']);
+        $amount1 = floatval($request->amount1); //IN USD
+        $amount2 = floatval($request->amount2); //IN BTC
+        $currency1 = $request->currency1; //USD
+        $currency2 = $request->currency2; //BTC
+        $status = intval($request->status);
 
         if ($currency2 != $order_currency) {
             edie("Currency Mismatch");
