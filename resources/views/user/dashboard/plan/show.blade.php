2078c90e8bf41928eee14e8f13e4693135be25f0
Shakeel Ahmed <shakeel2717@gmail.com>
2022-03-01 15:46:24 +0500

    user can activate with balance

--- resources/views/user/dashboard/plan/show.blade.php
+++ resources/views/user/dashboard/plan/show.blade.php
@@ -16,27 +16,11 @@
                 <b>${{ number_format($plan->price, 2) }}.</b> Plese click below Purchase now button to Confirm your
                 investment.</p>
             <div>
-                <div class="row">
-                    <div class="col-md-6 mx-auto">
-                        <div class="card">
-                            <div class="card-body text-left">
-                                <form action="{{ route('user.plan.store') }}" method="POST">
-                                    @csrf
-                                    <input type="hidden" name="plan_id" value="{{ $plan->id }}">
-                                    <div class="form-group">
-                                        <label for="method">Select Payment Method</label>
-                                        <select name="method" id="method" class="form-control">
-                                            <option value="BTC">BTC</option>
-                                            <option value="BUSD.BEP20">BUSD (BEP20)</option>
-                                            <option value="USDT.TRC20">USDT (TRC20)</option>
-                                        </select>
-                                    </div>
-                                    <input class="btn btn-hero btn-primary" type="submit" value="Purchase now">
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <form action="{{ route('user.plan.store') }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="plan_id" value="{{ $plan->id }}">
+                    <input class="btn btn-hero btn-primary" type="submit" value="Purchase now">
+                </form>
             </div>
         </div>
     </div>
