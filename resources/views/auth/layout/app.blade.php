6c01e63e7932d25d6cb1829f5e44bbca230116cd
Shakeel Ahmed <shakeel2717@gmail.com>
2022-03-04 02:23:22 +0500

    Whatsapp Descption button fixed

--- resources/views/auth/layout/app.blade.php
+++ resources/views/auth/layout/app.blade.php
@@ -1,44 +1,48 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-        <title>@yield('title') | {{ env('APP_DESC') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-        <meta name="description" content="{{ env('APP_DESC') }}">
-        <meta name="author" content="Asan Webs Development">
-        <meta name="robots" content="noindex, nofollow">
-        <meta property="og:title" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework">
-        <meta property="og:site_name" content="Dashmix">
-        <meta property="og:description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="">
-        <meta property="og:image" content="">
-        <link rel="shortcut icon" href="{{ asset('assets/brand/favi.png') }}">
-        <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/brand/favi.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/brand/favi.png') }}">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap">
-        <link rel="stylesheet" id="css-main" href="{{ asset('assets/css/dashmix.min.css') }}">
-    </head>
-    <body>
-        <div id="page-container">
-            <main id="main-container">
-                <div class="bg-image" style="background-image: url('assets/img/back.jpg');">
-                    <div class="row no-gutters justify-content-center bg-primary-dark-op">
-                        <div class="hero-static col-sm-8 col-md-6 col-xl-4 d-flex align-items-center p-2 px-sm-0">
-                            <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
-                                @yield('form')
-                            </div>
+    <title>@yield('title') | {{ env('APP_DESC') }}</title>
+
+    <meta name="description" content="{{ env('APP_DESC') }}">
+    <meta name="author" content="Asan Webs Development">
+    <meta name="robots" content="noindex, nofollow">
+
+    <meta property="og:title" content="{{ env('APP_DESC') }}">
+    <meta property="og:site_name" content="{{ env('APP_NAME') }}">
+    <meta property="og:description" content="{{ env('APP_DESC') }}">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+    <link rel="shortcut icon" href="{{ asset('assets/brand/favi.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/brand/favi.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/brand/favi.png') }}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap">
+    <link rel="stylesheet" id="css-main" href="{{ asset('assets/css/dashmix.min.css') }}">
+</head>
+
+<body>
+    <div id="page-container">
+        <main id="main-container">
+            <div class="bg-image" style="background-image: url('assets/img/back.jpg');">
+                <div class="row no-gutters justify-content-center bg-primary-dark-op">
+                    <div class="hero-static col-sm-8 col-md-6 col-xl-4 d-flex align-items-center p-2 px-sm-0">
+                        <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
+                            @yield('form')
                         </div>
                     </div>
                 </div>
-            </main>
-        </div>
-        <script src="{{ asset('assets/js/dashmix.core.min.js') }}"></script>
-        <script src="{{ asset('assets/js/dashmix.app.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
-        <script src="{{ asset('assets/js/pages/op_auth_signin.min.js') }}"></script>
-        <x-alert />
-    </body>
+            </div>
+        </main>
+    </div>
+    <script src="{{ asset('assets/js/dashmix.core.min.js') }}"></script>
+    <script src="{{ asset('assets/js/dashmix.app.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/op_auth_signin.min.js') }}"></script>
+    <x-alert />
+</body>
+
 </html>
