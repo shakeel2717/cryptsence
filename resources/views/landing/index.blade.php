ce67a4e2cccec87283d2fd23996d668e7f15e7d9
Shakeel Ahmed <shakeel2717@gmail.com>
2022-03-10 18:50:30 +0500

    chart landing page update

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -208,19 +208,24 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <div
-                        style="height:100vh; background-color: #FFFFFF; overflow:scroll; box-sizing: border-box; border: 1px solid #56667F; border-radius: 4px; text-align: right; line-height:14px; font-size: 12px; font-feature-settings: normal; text-size-adjust: 100%; box-shadow: inset 0 -20px 0 0 #56667F; padding: 0px; margin: 0px; width: 100%;">
-                        <div style="height:5959px; padding:0px; margin:0px; width: 100%;"><iframe
-                                src="https://widget.coinlib.io/widget?type=full_v2&theme=light&cnt=100&pref_coin_id=1505&graph=yes"
-                                width="100%" height="5955px" scrolling="auto" marginwidth="0" marginheight="0"
-                                frameborder="0" border="0" style="border:0;margin:0;padding:0;"></iframe></div>
-                        <div
-                            style="color: #FFFFFF; line-height: 14px; font-weight: 400; font-size: 11px; box-sizing: border-box; padding: 2px 6px; width: 100%; font-family: Verdana, Tahoma, Arial, sans-serif;">
-                            <a href="https://coinlib.io" target="_blank"
-                                style="font-weight: 500; color: #FFFFFF; text-decoration:none; font-size:11px">Cryptocurrency
-                                Prices</a>&nbsp;by Coinlib
-                        </div>
+                    <!-- TradingView Widget BEGIN -->
+                    <div class="tradingview-widget-container mx-auto">
+                        <div class="tradingview-widget-container__widget"></div>
+                        <div class="tradingview-widget-copyright"></div>
+                        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-screener.js" async>
+                            {
+                                "width": "100%",
+                                "height": "100vh",
+                                "defaultColumn": "overview",
+                                "screener_type": "crypto_mkt",
+                                "displayCurrency": "USD",
+                                "colorTheme": "light",
+                                "locale": "en",
+                                "isTransparent": false
+                            }
+                        </script>
                     </div>
+                    <!-- TradingView Widget END -->
                 </div>
             </div>
         </div>
