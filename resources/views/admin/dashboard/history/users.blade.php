c4b147b73d812613aa8d5a3efab62bc7b2e8f786
Shakeel Ahmed <79254929+shakeel2717@users.noreply.github.com>
2022-08-30 00:20:08 +0500

    users update dleete system

--- resources/views/admin/dashboard/history/users.blade.php
+++ resources/views/admin/dashboard/history/users.blade.php
@@ -45,8 +45,6 @@
                                         <td class="text-center text-capitalize">{{ $user->username }}</td>
                                         @if(ctse($user->id) < 1 && myPlanCount($user->id) < 1)
                                         <td class="text-center text-capitalize"><a href="{{ route('admin.history.user.delete',['id' => $user->id]) }}" class="btn btn-danger text-white">{{ ctse($user->id) }} + {{ myPlanCount($user->id) }}</a></td>
-                                        @else
-                                        <td class="text-center text-capitalize"><a href="# class="btn btn-theme text-white">Active</a></td>
                                         @endif
                                         <td class="text-center text-capitalize">{{ $user->email }}</td>
                                         <td class="text-center">${{ number_format(balance($user->id), 2) }}</td>
